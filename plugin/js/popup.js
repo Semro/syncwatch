ba64f7c319f915a393c3cce52cb1857ec70c0ed8
Semro <semro@outlook.com>
2018-02-07 04:25:21 +0300

    A lot of changes

--- plugin/js/popup.js
+++ plugin/js/popup.js
@@ -1,7 +1,31 @@
 var form = document.forms.connect;
 var con = form.elements.connect;
 
+chrome.storage.sync.get('name', function (result)
+{
+	form.elements.name.value = result.name;
+});
+
+chrome.storage.sync.get('room', function (result)
+{
+	form.elements.room.value = result.room;
+});
+
 con.onclick = function()
 {
-  console.log("name: "+form.elements.name.value+" room: "+form.elements.room.value);
+	var name = form.elements.name.value;
+	var room = form.elements.room.value;
+
+ 	chrome.storage.sync.set(
+ 	{
+ 		'name': name,
+ 		'room': room
+ 	});
+
+  chrome.runtime.sendMessage(
+  {
+    from: 'join',
+    name: name,
+    room: room
+  });
 }
