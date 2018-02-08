43c7ebcf9acd10c92d4dcbbaf8e7841f803ebe52
unknown <semenrozhkov@mail.ru>
2018-02-08 18:21:22 +0300

    Some refactoring

--- plugin/js/background.js
+++ plugin/js/background.js
@@ -1,14 +1,27 @@
-console.log('background.js');
+'use strict';
 
 var socket = io.connect('https://syncevent.herokuapp.com');
 var recieved = false;
-var pp = false;
 var contentTabId;
+
+function broadcast(event, elem, time)
+{
+  if (!recieved)
+  {
+    socket.json.send(
+    {
+      'event': event,
+      'elem': elem,
+      'time': time
+    });
+    console.log('broadcast: '+event);
+  }
+  else recieved = false;
+}
 
 socket.on('message', function (msg)
 {
   recieved = true;
-  console.log('socket.on: '+msg.event);
   chrome.tabs.sendMessage(contentTabId,
 	{
 		from: 'background',
@@ -16,22 +29,8 @@ socket.on('message', function (msg)
 		elem: msg.elem,
 		time: msg.time
 	});
+  console.log('socket.on: '+msg.event);
 });
-
-function broadcast(event, elem, time)
-{
-  if (!recieved)
-  {
-    console.log('broadcast: '+event);
-    socket.json.send(
-    {
-      'event': event,
-      'elem': elem,
-      'time': time
-    });
-  }
-  else recieved = false;
-}
 
 chrome.runtime.onMessage.addListener( function(msg, sender)
 {
@@ -57,3 +56,5 @@ chrome.runtime.onMessage.addListener( function(msg, sender)
 		console.log(msg.res);
 	}
 });
+
+console.log('background.js');
