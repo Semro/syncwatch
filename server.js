f9066ac9ffc2068cdccc685840423460208679c1
unknown <semenrozhkov@mail.ru>
2018-03-01 02:33:17 +0300

    ping

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+var http = require('http');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+	.use((req, res) => res.sendFile(INDEX) )
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 
@@ -35,18 +36,30 @@ io.on('connection', function (socket)
 		console.log(rooms[socket.id]+': '+users[socket.id]+' '+msg.event+' '+msg.time);
 	});
 
-	socket.on('ping', function()
+	var intervalId = setInterval(function() 
 	{
-		socket.emit('pong');
-	})
+		http.get("http://syncevent.herokuapp.com");
+		socket.emit('ping', function()
+		{
+			var sendTime = Date.now();
+		})
+	}, 10000);  // 270000
 
-	socket.on('alive', function(data)
+	socket.on('pong', function(data)
 	{
-		console.log('alive: '+data.name);
+		var receiveTime = Date.now();
+		var pingTime = recieveTime - sendTime;
+		console.log(data.name+' ping: '+pingTime+' ms');
 	})
 
 	socket.on('disconnect', function () 
 	{
 		console.log(rooms[socket.id]+': '+users[socket.id]+' disconnected');
+		users.splice(socket.id, 1);
+		if (!users.length)
+		{
+			clearInterval(intervalId);
+			console.log("All disconnected!");
+		}
 	});
 });
