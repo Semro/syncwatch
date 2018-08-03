2e6af3a3dc2712e796f25fe5634ea38654562daf
Semro <semro@outlook.com>
2018-08-04 01:35:07 +0300

    Changed UI in popup.html a little bit.

--- plugin/js/popup.js
+++ plugin/js/popup.js
@@ -3,6 +3,32 @@
 var form = document.forms.connect;
 var connect = form.elements.connect;
 var disconnect = form.elements.disconnect;
+var console =
+{
+	log: function(res)
+	{
+		chrome.runtime.sendMessage({from: 'console', res: 'popup.js: '+res});
+	}
+}
+
+function updateStatus(newStatus)
+{
+	if (newStatus != undefined)
+	{
+		chrome.storage.local.set(
+		{
+			'status': newStatus
+		});
+		document.getElementById('status').innerHTML = newStatus;
+	}
+	else
+	{
+		chrome.storage.local.get('status', function (result)
+		{
+			document.getElementById('status').innerHTML = result.status;
+		});
+	}
+}
 
 chrome.storage.sync.get('name', function (result)
 {
@@ -42,8 +68,12 @@ chrome.runtime.onMessage.addListener( function(msg, sender)
 {
 	if (msg.from == 'status')
 	{
-		document.getElementById('status').innerHTML = 'status: '+msg.status;
+		updateStatus(msg.status);
 	}
 });
 
+window.onload = function()
+{
+	updateStatus();
+}
 //chrome.runtime.sendMessage({from: 'console', res: 'res'});
