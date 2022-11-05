07608647dde24d57a72f8b1d644987bc1b5d1d11
Semyon Rozhkov <semro@outlook.com>
2022-11-05 21:44:10 +0300

    chore: run eslint

--- plugin/js/options.js
+++ plugin/js/options.js
@@ -17,9 +17,8 @@ function saveOptions() {
 
 function restoreOptions() {
   document.getElementById('options_server').innerText = chrome.i18n.getMessage('options_server');
-  document.getElementById('options_server_warning').innerText = chrome.i18n.getMessage(
-    'options_server_warning'
-  );
+  document.getElementById('options_server_warning').innerText =
+    chrome.i18n.getMessage('options_server_warning');
   document.getElementById('save').value = chrome.i18n.getMessage('options_button_save');
 
   chrome.storage.sync.get('connectionUrl', (obj) => {
