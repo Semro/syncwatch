2f3c83c99b1c04faaa4d091834869d74c759cd7f
Semyon Rozhkov <semro@outlook.com>
2023-12-12 23:02:21 +0300

    chore: init template

---
+++ packages/syncwatch-extension/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+export default defineConfig({
+  plugins: [
+    webExtension({
+      manifest: generateManifest,
+      watchFilePaths: ["package.json", "manifest.json"],
+    }),
+  ],
+});
