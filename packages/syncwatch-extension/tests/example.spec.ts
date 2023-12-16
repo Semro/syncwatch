7866389e7eede5bb7391481f33f54453a8d617ad
Semyon Rozhkov <semro@outlook.com>
2023-12-17 00:57:07 +0300

    test(ext): add sample extension test

--- packages/syncwatch-extension/tests/example.spec.ts
+++ packages/syncwatch-extension/tests/example.spec.ts
@@ -1,18 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from './fixtures';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('popup page', async ({ page, extensionId }) => {
+  await page.goto(`chrome-extension://${extensionId}/popup.html`);
+  const title = await page.title();
+  await expect(title).toBe('SyncWatch');
 });
---
+++ packages/syncwatch-extension/tests/fixtures.ts
@@ -0,0 +1,35 @@
+import { test as base, chromium, type BrowserContext } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname);
+
+export const test = base.extend<{
+  context: BrowserContext;
+  extensionId: string;
+}>({
+  context: async ({}, use) => {
+    const pathToExtension = path.join(__dirname, '../dist');
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.backgroundPages();
+    if (!background) background = await context.waitForEvent('backgroundpage');
+
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+export const expect = test.expect;
