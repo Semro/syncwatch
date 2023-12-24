6f630d2090086fc376164b05c0cb4659a9d7c850
Semyon Rozhkov <semro@outlook.com>
2023-12-24 19:52:30 +0300

    test: convert comments to steps

--- packages/syncwatch-extension/tests/example.spec.ts
+++ packages/syncwatch-extension/tests/example.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from './fixtures';
+import { Page } from '@playwright/test';
+import { test, expect, describe } from './fixtures';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -20,55 +21,63 @@ test('screenshot_option', async ({ page, extensionId }) => {
   await expect(page.locator('#main')).toHaveScreenshot();
 });
 
-const initialState = (page) => {
-  return Promise.all([
-    expect(page.locator('#shared')).toBeVisible({ visible: false }),
-    expect(page.locator('#usersList')).toBeVisible({ visible: false }),
-    expect(page.locator('#status')).toHaveText('status: disconnected'),
-  ]);
+const initialState = (page: Page) => {
+  return test.step('Initial state', () => {
+    return Promise.all([
+      expect(page.locator('#shared')).toBeVisible({ visible: false }),
+      expect(page.locator('#usersList')).toBeVisible({ visible: false }),
+      expect(page.locator('#status')).toHaveText('status: disconnected'),
+    ]);
+  });
 };
 
-test('connect to the server', async ({ page, extensionId, context }) => {
+test('user scenario', async ({ page, extensionId, context }) => {
   const userName = 'User1';
   const serverUrl = `http://localhost:${process.env.SERVER_PORT}`;
   const video = 'https://www.w3.org/2010/05/video/mediaevents.html';
 
-  // Change server URL
-  await page.goto(`chrome-extension://${extensionId}/options.html`);
+  await test.step('Change server URL', async () => {
+    await page.goto(`chrome-extension://${extensionId}/options.html`);
 
-  await page.locator('#serverUrl').fill(serverUrl);
-  await page.getByRole('button', { name: 'save' }).click();
+    await page.locator('#serverUrl').fill(serverUrl);
+    await page.getByRole('button', { name: 'save' }).click();
+  });
 
-  // Initial State
-  await page.goto(`chrome-extension://${extensionId}/popup.html`);
-  await initialState(page);
+  await test.step('Open popup', async () => {
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    await initialState(page);
+  });
 
-  // Connect to the server
-  await page.getByPlaceholder('Type your name').fill(userName);
-  await page.getByPlaceholder('Type room name').fill('RoomName');
+  await test.step('Connect to the server', async () => {
+    await page.getByPlaceholder('Type your name').fill(userName);
+    await page.getByPlaceholder('Type room name').fill('RoomName');
 
-  await page.getByRole('button', { name: 'connect' }).click();
-  await expect(page.locator('#status')).toHaveText('status: connected');
-  await expect(page.locator('#usersList')).toBeVisible();
-  await expect(page.locator('#usersList')).toHaveText(userName);
+    await page.getByRole('button', { name: 'connect' }).click();
+    await expect(page.locator('#status')).toHaveText('status: connected');
+    await expect(page.locator('#usersList')).toBeVisible();
+    await expect(page.locator('#usersList')).toHaveText(userName);
+  });
 
-  // Share a video
-  await page.goto(`chrome-extension://${extensionId}/popup.html`);
+  await test.step('Share a video', async () => {
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
 
-  const pageVideo = await context.newPage();
-  await pageVideo.goto(video);
+    const pageVideo = await context.newPage();
+    await pageVideo.goto(video);
 
-  await page.getByRole('button', { name: 'share' }).click();
-  await expect(page.locator('#shared')).toHaveAttribute('href', video);
-  await expect(page.locator('#shared')).toBeVisible();
+    await page.getByRole('button', { name: 'share' }).click();
+    await expect(page.locator('#shared')).toHaveAttribute('href', video);
+    await expect(page.locator('#shared')).toBeVisible();
+  });
 
-  // Open a shared video
-  await page.locator('#shared').click();
-  const pagePromise = page.context().waitForEvent('page', (p) => p.url() === video);
-  const newPage = await pagePromise;
-  await expect(newPage).toHaveURL(video);
+  await test.step('Open a shared video', async () => {
+    await page.locator('#shared').click();
+    const pagePromise = page.context().waitForEvent('page', (p) => p.url() === video);
+    const newPage = await pagePromise;
+    await expect(newPage).toHaveURL(video);
+  });
 
-  // Disconnect from the server
-  await page.getByRole('button', { name: 'connect' }).click();
-  await initialState(page);
+  await test.step('Disconnect from the server', async () => {
+    await page.getByRole('button', { name: 'connect' }).click();
+    await initialState(page);
+  });
 });
