49bb5cdda12cf525592bc6fa1ddd332d88623fa8
Semyon Rozhkov <semro@outlook.com>
2024-02-23 15:52:41 +0300

    chore: add build folder to gitignore

--- packages/syncwatch-extension/dist/js/players/netflix/loadNetflix.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";const e=document.createElement("script");e.src=chrome.runtime.getURL("js/players/netflix/netflix.js"),e.onload=()=>{(void 0).remove()},(document.head||document.documentElement).appendChild(e)})();
