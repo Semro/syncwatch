0666781569691f88b8aab21732a8d19f9b90384a
Semyon Rozhkov <semro@outlook.com>
2024-02-24 15:39:54 +0300

    test: fix fixture for MV3

--- packages/syncwatch-extension/tests/fixtures.ts
+++ packages/syncwatch-extension/tests/fixtures.ts
@@ -30,8 +30,13 @@ export const test = base.extend<{
     await context.close();
   },
   extensionId: async ({ context }, use) => {
-    let [background] = context.backgroundPages();
-    if (!background) background = await context.waitForEvent('backgroundpage');
+    // for manifest v2:
+    // let [background] = context.backgroundPages();
+    // if (!background) background = await context.waitForEvent('backgroundpage');
+
+    // for manifest v3:
+    let [background] = context.serviceWorkers();
+    if (!background) background = await context.waitForEvent('serviceworker');
 
     const extensionId = background.url().split('/')[2];
     await use(extensionId);
