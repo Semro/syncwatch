b3cdb95c0ade6f1ec2adb78d316d707655c5ba99
Semyon Rozhkov <semro@outlook.com>
2024-03-31 17:52:27 +0300

    refactor(types): popup.js

--- packages/syncwatch-extension/src/js/popup.ts
+++ packages/syncwatch-extension/src/js/popup.ts
@@ -1,19 +1,39 @@
-const roomElement = document.getElementById('room');
-const nameElement = document.getElementById('name');
-const connectElement = document.getElementById('connect');
-const shareElement = document.getElementById('share');
-const sharedElement = document.getElementById('shared');
-const usersListTitle = document.getElementById('usersListTitle');
-const usersList = document.getElementById('usersList');
-const errorElement = document.getElementById('error');
+import { Share, User, UserList } from '../../../syncwatch-types/types';
 
-function getData(type) {
+type BaseRuntimeMessage<From extends string> = {
+  from: From;
+};
+
+type MessageStatus = BaseRuntimeMessage<'status'> & { status: string };
+type MessageShare = BaseRuntimeMessage<'share'> & { data: Share | undefined };
+type MessageSendUsersList = BaseRuntimeMessage<'sendUsersList'> & { list: UserList };
+type MessageSendError = BaseRuntimeMessage<'sendError'> & { error: string };
+type MessageSendUser = BaseRuntimeMessage<'sendUser'> & { data: User };
+
+type RuntimeMessage =
+  | MessageStatus
+  | MessageShare
+  | MessageSendUsersList
+  | MessageSendError
+  | MessageSendUser;
+
+const roomInputElement = document.getElementById('room') as HTMLInputElement;
+const nameInputElement = document.getElementById('name') as HTMLInputElement;
+const connectButtonElement = document.getElementById('connect') as HTMLInputElement;
+const shareInputElement = document.getElementById('share') as HTMLInputElement;
+const shareAnchorElement = document.getElementById('shared') as HTMLAnchorElement;
+const usersListTitleElement = document.getElementById('usersListTitle') as HTMLDivElement;
+const usersListElement = document.getElementById('usersList') as HTMLElement;
+const errorElement = document.getElementById('error') as HTMLDivElement;
+const statusElement = document.getElementById('status') as HTMLDivElement;
+
+function getData(type: string) {
   chrome.runtime.sendMessage({
     from: `get${type}`,
   });
 }
 
-function getFaviconFromUrl(url) {
+function getFaviconFromUrl(url: string) {
   let position = 0;
   for (let i = 0; i < 3; i++) {
     position = url.indexOf('/', position);
@@ -22,66 +42,66 @@ function getFaviconFromUrl(url) {
   return `${url.substring(0, position)}favicon.ico`;
 }
 
-function displayElements(display) {
-  shareElement.style.display = display;
-  usersListTitle.style.display = display;
-  usersList.style.display = display;
+function displayElements(display: string) {
+  shareInputElement.style.display = display;
+  usersListTitleElement.style.display = display;
+  usersListElement.style.display = display;
 }
 
-shareElement.onclick = () => {
+shareInputElement.onclick = () => {
   chrome.runtime.sendMessage({ from: 'popupShare' });
 };
 
-sharedElement.onclick = () => {
-  chrome.runtime.sendMessage({ from: 'popupOpenVideo', data: { url: sharedElement.href } });
+shareAnchorElement.onclick = () => {
+  chrome.runtime.sendMessage({ from: 'popupOpenVideo', data: { url: shareAnchorElement.href } });
   return false;
 };
 
-chrome.runtime.onMessage.addListener((msg) => {
+chrome.runtime.onMessage.addListener((msg: RuntimeMessage) => {
   if (msg.from === 'status') {
     if (msg.status === 'connect') {
-      connectElement.value = chrome.i18n.getMessage('popup_button_disconnect');
-      connectElement.onclick = () => {
+      connectButtonElement.value = chrome.i18n.getMessage('popup_button_disconnect');
+      connectButtonElement.onclick = () => {
         chrome.runtime.sendMessage({ from: 'disconnect' });
       };
       displayElements('block');
     } else {
-      connectElement.value = chrome.i18n.getMessage('popup_button_connect');
-      connectElement.onclick = () => {
+      connectButtonElement.value = chrome.i18n.getMessage('popup_button_connect');
+      connectButtonElement.onclick = () => {
         errorElement.style.display = 'none';
         const user = {
-          name: nameElement.value,
-          room: roomElement.value,
+          name: nameInputElement.value,
+          room: roomInputElement.value,
         };
         chrome.runtime.sendMessage({ from: 'join', data: user });
-        connectElement.value = `${chrome.i18n.getMessage('popup_button_connecting')}...`;
+        connectButtonElement.value = `${chrome.i18n.getMessage('popup_button_connecting')}...`;
       };
       displayElements('none');
-      sharedElement.style.display = 'none';
+      shareAnchorElement.style.display = 'none';
     }
-    document.getElementById('status').innerText = `${chrome.i18n.getMessage('popup_status')}:\
+    statusElement.innerText = `${chrome.i18n.getMessage('popup_status')}:\
 													   ${chrome.i18n.getMessage(`socket_event_${msg.status}`)}`;
   }
   if (msg.from === 'share') {
-    if (msg.data !== null) {
-      sharedElement.href = msg.data.url;
-      sharedElement.innerText = '';
+    if (msg.data) {
+      shareAnchorElement.href = msg.data.url;
+      shareAnchorElement.innerText = '';
       const img = document.createElement('img');
       const span = document.createElement('span');
       img.style.height = '16px';
       img.src = getFaviconFromUrl(msg.data.url);
       span.innerText = msg.data.title;
-      sharedElement.appendChild(img);
-      sharedElement.appendChild(span);
-      sharedElement.style.display = 'block';
+      shareAnchorElement.appendChild(img);
+      shareAnchorElement.appendChild(span);
+      shareAnchorElement.style.display = 'block';
     }
   }
   if (msg.from === 'sendUsersList') {
-    usersList.innerText = '';
-    for (const key in msg.list) {
+    usersListElement.innerText = '';
+    for (const user of Object.values(msg.list)) {
       const li = document.createElement('li');
-      li.innerText = msg.list[key];
-      usersList.appendChild(li);
+      li.innerText = user;
+      usersListElement.appendChild(li);
     }
   }
   if (msg.from === 'sendError') {
@@ -89,15 +109,15 @@ chrome.runtime.onMessage.addListener((msg) => {
     errorElement.innerText = chrome.i18n.getMessage(msg.error);
   }
   if (msg.from === 'sendUser' && msg.data) {
-    nameElement.value = msg.data.name;
-    roomElement.value = msg.data.room;
+    nameInputElement.value = msg.data.name;
+    roomInputElement.value = msg.data.room;
   }
 });
 
-nameElement.placeholder = chrome.i18n.getMessage('popup_input_name');
-roomElement.placeholder = chrome.i18n.getMessage('popup_input_room');
-shareElement.value = chrome.i18n.getMessage('popup_button_share');
-usersListTitle.innerText = `${chrome.i18n.getMessage('popup_usersInRoom')}:`;
+nameInputElement.placeholder = chrome.i18n.getMessage('popup_input_name');
+roomInputElement.placeholder = chrome.i18n.getMessage('popup_input_room');
+shareInputElement.value = chrome.i18n.getMessage('popup_button_share');
+usersListTitleElement.innerText = `${chrome.i18n.getMessage('popup_usersInRoom')}:`;
 
 const typesOfData = ['User', 'Status', 'UsersList', 'Share'];
 for (const val of typesOfData) getData(val);
